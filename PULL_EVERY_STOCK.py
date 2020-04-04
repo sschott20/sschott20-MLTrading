601f196919c63820b1ae65064e854d6ea37447eb
sschott <sschott20@students.hopkins.edu>
2020-04-04 00:42:34 -0400

    changes

--- PULL_EVERY_STOCK.py
+++ PULL_EVERY_STOCK.py
@@ -1,44 +1,61 @@
 import math
 import os
-import shutil
-import time
 import urllib.request
+from urllib.error import HTTPError
 import time
 import pandas as pd
 
-# period1 and period2 are Unix time stamps for your start and end date
 # interval is the data retrieval interval (this can be either 1d, 1w or 1m)
 
 # NEED RETURN AND OUTSTANDING NUMBER OF SHARES
 
 
 def pull_all_data(src, start_idx=0):
+    file_length = 0
+
+    with open("ALL_DATA/Lists/missed.txt", "a") as missed_file:
+        missed_file.truncate(0)
+        missed_file.close()
+
     print("PULLING DATA FROM: " + src)
+
     with open(src, "r") as file:
         file_lines = file.readlines()
         file_length = len(file_lines)
+
         for i in range(start_idx, file_length):
-            company = file_lines[i].strip()
-            ticker = company.split()
-            # print(ticker[0])
-            pull_specific_stocks([ticker[0]])
+            symbol = file_lines[i].strip()
+            screen = True
+            while screen:
+                try:
+                    print("Downloading historical data for: " + symbol)
+                    pull_historical_data(symbol)
+                    screen = False
+                except HTTPError as e:
+                    if e.code == 401:
+                        print(f"\nWaiting 120 Seconds, index = {i}")
+                        time.sleep(120)
+                    else:
+                        print(e)
+                        with open("ALL_DATA/Lists/missed.txt", "a") as missed_file:
+                            missed_file.write(symbol)
+                            missed_file.write("\n")
+                            missed_file.close()
+                        screen = False
 
-
-def pull_specific_stocks(stocks):
-    """Download data of specific stocks from yahoo finance"""
-    for stock in stocks:
-        print("Downloading historical data for: " + stock)
-        pull_historical_data(stock)
+    return file_length
 
 
 current_time = str(math.floor(time.time()))
 # Times:
+# 2020-01-01 : 1577836800
 # 2019-01-01 : 1546300800
 # 2000-01-01 : 946684800
+#
 
 
 def make_url(
-    ticker_symbol, period1="1546300800", period2=current_time, interval="1d",
+    ticker_symbol, period1="1577836800", period2=current_time, interval="1d",
 ):
     return "https://query1.finance.yahoo.com/v7/finance/download/{}?period1={}&period2={}&interval={}&events=history".format(
         ticker_symbol, period1, period2, interval
@@ -51,39 +68,55 @@ def make_filename(ticker_symbol):
 
 def pull_historical_data(ticker_symbol):
     filename = make_filename(ticker_symbol)
-    try:
-        urllib.request.urlretrieve(make_url(ticker_symbol), filename)
 
-        df = pd.read_csv(
-            filename,
-            index_col="Date",
-            parse_dates=True,
-            # usecols=["Date", "Adj Close"],
-            na_values=["nan"],
-        )
-        df_temp = df.copy()
-        df_temp = df_temp["Adj Close"]
+    urllib.request.urlretrieve(
+        make_url(ticker_symbol), os.path.join(os.getcwd(), filename)
+    )
 
-        df_temp = df_temp.rename(columns={"Adj Close": "Daily Return"})
-        df_add = (df_temp[1:] / df_temp.values[:-1]) - 1
-        # print(df_add)
-        # df.join(df_add)
-        df["Daily Return"] = df_add
+    df = pd.read_csv(filename, index_col="Date", parse_dates=True, na_values=["nan"],)
+    df_temp = df.copy()
+    df_temp = df_temp["Adj Close"]
 
-        dst = os.getcwd() + "/ALL_DATA/Data"
+    df_temp = df_temp.rename(columns={"Adj Close": "Daily Return"})
+    df_add = (df_temp[1:] / df_temp.values[:-1]) - 1
 
-        df.to_csv(os.path.join(dst, filename), index="Date")
-        os.remove(filename)
+    df["Daily Return"] = df_add
 
-    except Exception as e:
-        print(e)
+    dst = os.getcwd() + "/ALL_DATA/Data"
+
+    df.to_csv(os.path.join(dst, filename), index="Date")
+    os.remove(filename)
+
+
+def get_PRN_data():
+    urllib.request.urlretrieve(
+        "https://query1.finance.yahoo.com/v7/finance/download/PRN?period1=1577836800&period2=1585872000&interval=1d&events=history",
+        "ALL_DATA/Data/_PRN.csv",
+    )
+    df = pd.read_csv(
+        "ALL_DATA/Data/_PRN.csv", index_col="Date", parse_dates=True, na_values=["nan"],
+    )
+    df_temp = df.copy()
+    df_temp = df_temp["Adj Close"]
+
+    df_temp = df_temp.rename(columns={"Adj Close": "Daily Return"})
+    df_add = (df_temp[1:] / df_temp.values[:-1]) - 1
+
+    df["Daily Return"] = df_add
+
+    dst = os.getcwd() + "/ALL_DATA/Data"
+
+    df.to_csv(os.path.join(dst, "_PRN.csv"), index="Date")
 
 
 if __name__ == "__main__":
-    # start_time = time.time()
 
-    # pull_all_data("ALL_DATA/Lists/NASDAQ.txt")
-    pull_all_data("ALL_DATA/Lists/ALL_SYMBOLS_TEST.txt")
-    # print(time.time()-start_time)
-    # stocks = ['BRK-B']
-    # pull_specific_stocks(stocks)
+    start_time = time.time()
+    get_PRN_data()
+    stop_index = pull_all_data("ALL_DATA/Lists/new_list.txt")
+    end_time = time.time()
+
+    print("\n--- TOTAL TIME ---")
+    print(f"{round(end_time-start_time, 4)} seconds\n")
+    print(f"--- AVERAGE TIME OVER {stop_index} STOCKS ---")
+    print(f"{round((end_time - start_time) / stop_index , 4)} SECONDS PER STOCK")
