8b8b92d7c71ada4fd33c349a236e01dcdcf26faf
sschott <sschott20@students.hopkins.edu>
2020-05-01 11:21:26 -0400

    market sim updating

--- Project 5 - Market Simulation/MarketSim.py
+++ Project 5 - Market Simulation/MarketSim.py
@@ -1,14 +1,26 @@
 import numpy as np
 import pandas as pd
 import os
+import sim_util as util
+import datetime as dt
 
 class MarketSim(object):
 
-    def __init__(self, portfolio_file, orders_file, start_date):
-        self.date = start_date
+    def __init__(self, portfolio_file, orders_file, start_date, current_date):
+        self.start_date = start_date
+        self.current_date = current_date
         self.portfolio_file = portfolio_file
         self.orders_file = orders_file
 
+        t = pd.date_range(start_date, current_date)
+
+        self.dates = util.get_data(["SPY"], t).index
+        print(self.dates)
+        try:
+            self.current_date_index = self.dates.get_loc(current_date)
+        except Exception as e:
+            try:
+                self.current_date_index = self.dates.get_loc()
         self.update_files()
 
     def update_files(self):
@@ -26,10 +38,23 @@ class MarketSim(object):
         portfolio = portfolio.groupby("Symbol", axis=0).sum()
 
         os.remove(self.portfolio_file)
-        with open(self.orders_file, "w") as f:
-            f.truncate(0)
+        # with open(self.orders_file, "w") as f:
+        #     f.truncate(0)
 
         portfolio.to_csv(self.portfolio_file)
 
-    def end_day(self):
-        pass
+    def update(self, time_scale="1d", step_size=1):
+        stocklist = []
+
+        # self.current_date = self.dates[]
+        dates = pd.date_range(self.start_date, self.current_date)
+
+        with open("portfolio.csv", "r") as f:
+            portfolio = f.read().splitlines()
+            portfolio = [line.split(",") for line in portfolio]
+
+            portfolio.remove(['Symbol', 'Stocks'])
+            stocklist = [line[0] for line in portfolio]
+
+        # print(util.get_data(stocklist, dates))
+        return util.get_data(stocklist,dates)
--- Project 5 - Market Simulation/TestSim.py
+++ Project 5 - Market Simulation/TestSim.py
@@ -2,11 +2,14 @@
 import pandas as pd
 import numpy as np
 import MarketSim as ms
-if __name__ == "__main__":
 
-    current_date = "2019-01-01"
+if __name__ == "__main__":
+    start_date = "2019-01-24"
+    current_date = "2019-02-02"
     portfolio_file = "portfolio.csv"
     orders_file = "orders.csv"
 
-    Sim = ms.MarketSim(portfolio_file, orders_file, "2019-01-01")
+    Sim = ms.MarketSim(portfolio_file, orders_file, start_date, current_date)
     Sim.order_stonks()
+    df = Sim.update()
+    # print(df)
