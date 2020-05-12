9f67429527e0e2cbbf6a0869acb566ab56a495ce
sschott <sschott20@students.hopkins.edu>
2020-05-11 21:02:39 -0400

    strict trading

---
+++ Project 8 - Bolinger Bot/utility.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from historical_data import *
+import matplotlib
+matplotlib.use("TKAgg")
+from matplotlib import pyplot as plt
+def symbol_to_path(symbol):
+    """Return CSV file path given ticker symbol.
+    If CSV is not already downloaded, will download from yahoo finance """
+
+    base_dir = os.path.dirname(os.getcwd())
+    base_dir = os.path.join(base_dir, "data")
+
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates, removeSPY=True, colname="Adj Close"):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    # remove_SPY = False
+    if "SPY" not in symbols:  # add SPY for reference, if absent
+        # remove_SPY = True
+        symbols = ["SPY"] + list(
+            symbols
+        )  # handles the case where symbols is np array of 'object'
+
+    for symbol in symbols:
+        df_temp = pd.read_csv(
+            symbol_to_path(symbol),
+            index_col="Date",
+            parse_dates=True,
+            usecols=["Date", colname],
+            na_values=["nan"],
+        )
+        df_temp = df_temp.rename(columns={colname: symbol})
+        df = df.join(df_temp)
+
+        if symbol == "SPY":  # drop dates SPY did not trade
+            df = df.dropna(subset=["SPY"])
+
+    if removeSPY:
+        df = df.drop(columns="SPY")
+
+    return df
+
+def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
+    """Plot stock prices with a custom title and meaningful axis labels."""
+    ax = df.plot(title=title, fontsize=12)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
